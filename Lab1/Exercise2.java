a61196eef9f61bcc8167d653778025e4e08a2a39
Teresa <teresa.alice.alves@gmail.com>
2020-05-15 10:00:45 -0700

    Exercise2 Submission

---
+++ Lab1/Exercise2.java
@@ -0,0 +1,12 @@
+public class Exercise2 {
+	public static void main(String[] args){
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==         Student  Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+        System.out.println("Name \t Lab \t Bonus \t Total");
+        System.out.println("---- \t --- \t ----- \t -----");
+        System.out.println("Tess \t 45 \t 8 \t 53");
+        System.out.println("Joanne \t 40 \t 5 \t 45");
+        System.out.println("Ruan \t 43 \t 7 \t 50");
+	}
+}
