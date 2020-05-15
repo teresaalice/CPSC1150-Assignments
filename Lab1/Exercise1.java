06c8652ce05698ca04f497ebc9b6a94b1814f9d0
Teresa <teresa.alice.alves@gmail.com>
2020-05-15 09:29:47 -0700

    Exercise1 submission

---
+++ Lab1/Exercise1.java
@@ -0,0 +1,9 @@
+public class Exercise1{
+	public static void main(String[] args){
+        System.out.println("T T T T T  A A A A   A A A A   S S S S");
+        System.out.println("    T      A     A   A     A   S      ");
+        System.out.println("    T      A A A A   A A A A   S S S S");
+        System.out.println("    T      A     A   A     A         S");
+        System.out.println("    T      A     A   A     A   S S S S");
+	}
+}
