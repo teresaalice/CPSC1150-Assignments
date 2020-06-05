2909828ae5179dcfb2bdccfe28e8166c50a93ac9
Teresa <teresa.alice.alves@gmail.com>
2020-06-04 17:00:08 -0700

    Logic corrections for Quadratic.java

--- Lab4/Quadratic.java
+++ Lab4/Quadratic.java
@@ -8,22 +8,30 @@ public class Quadratic {
         System.out.println();
         System.out.println ("Quadratic Equation Calculator");
         System.out.println();
-        //Prompts user for a string
+        //Prompts user for 3 numbers, each divided by a space or enter
         System.out.print("Please enter numerical values for a, b and c respectively:" );
-        Double aInput = scan.nextDouble();
-        Double bInput = scan.nextDouble();
-        Double cInput = scan.nextDouble();
+        double aInput = scan.nextDouble();
+        double bInput = scan.nextDouble();
+        double cInput = scan.nextDouble();
 
-        // Calculates the value of
-        Double deltaValue = bInput*bInput - 4*aInput*cInput;
+        // Calculates the value of b² - 4ac, which determines how many roots the equation has
+        double deltaValue = Math.pow(bInput, 2) - 4*aInput*cInput;
+        double root;
+        double secondRoot;
 
-        if (deltaValue > 0){
-            Double firstRoot = (-bInput + Math.pow(deltaValue, 1/2)) / (2 * aInput);
-            Double secondRoot = (-bInput - Math.pow(deltaValue, 1/2)) / (2 * aInput);
+        //In case a is equal to zero, uses the x = -(c/b) formula to avoid division by 0
+        //otherwise calculates the roots for each case
+        if(aInput == 0){
+            root = -(cInput/bInput);
+            System.out.print("This equation has one real root. x = " + root);
+        }
+        else if (deltaValue > 0){
+            root = (-bInput + Math.sqrt(deltaValue)) / (2 * aInput);
+            secondRoot = (-bInput - Math.sqrt(deltaValue)) / (2 * aInput);
 
-            System.out.print("This equation has two real roots. x1 = " + firstRoot + " and x2 = " + secondRoot);
+            System.out.print("This equation has two real roots. x1 = " + root + " and x2 = " + secondRoot);
         } else if(deltaValue == 0){
-            Double root = (-bInput) / (2 * aInput);
+            root = (-bInput) / (2 * aInput);
             System.out.print("This equation has one real root. x = " + root);
         } else{
             System.out.print("This equation has no real roots.");
