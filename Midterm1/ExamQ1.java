46027470bd59d3e7a082db8ef5d0344222ac5aa4
Teresa <teresa.alice.alves@gmail.com>
2020-06-11 08:44:18 -0700

    Innitial commit for ExamQ1.java for Midterm 1

---
+++ Midterm1/ExamQ1.java
@@ -0,0 +1,37 @@
+// ************************************************
+//   Program name: ExamQ1.java from TSilva100350471.zip
+//   Student name: Teresa Alice Alves e Silva
+//   Student ID: 100350471
+//   Session: W01
+//   Date: 06/11/2020
+//   Program Description: Reads one positive integer N
+//   finds and prints the largest odd number that is smaller than N
+//   and that is evenly divisible by 5 but not evenly divisible by 3
+//
+//
+// ************************************************
+
+import java.util.Scanner;
+
+public class ExamQ1 {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a positive integer: ");
+        int userInput = input.nextInt();
+        
+        int number = 1;
+        int biggestResult = 0;
+
+        while (number < userInput)
+        {
+            if ((number % 2 != 0) && (number % 5 == 0) && (number % 3 != 0))
+            {
+                biggestResult = number;
+            }
+            number++;
+        }
+
+        System.out.println("The biggest odd number smaller than " + userInput + " that is divisible by 5 but not by 3 is: " + biggestResult);
+        input.close();
+    }
+}
